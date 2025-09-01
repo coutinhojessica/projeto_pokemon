19d51c245ec92050c66d42bd7566f086a5dbdd16
Jéssica coutinho <jessicamaiara.coutinho@gmail.com>
2025-09-01 07:33:18 -0300

    Correção no Javascript.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,20 @@ const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 
 const body = document.querySelector("body");
 
-const imagemBotaoTrocaTema = document.querySelector(".img-btn")
+const imagemBotaoTrocaTema = document.querySelector(".img-btn");
 
 botaoAlterarTema.addEventListener("click", () => {
-    const modoEscuroAtivo = body.classList.contains("modo-escuro");
-    body.classList.toggle("modo-escuro");
-    if(modoEscuroAtivo){
-        imagemBotaoTrocaTema.setAttribute("src", "./assets/projeto-listagem-pokemon/src/imagens/sun.png");
-    }else{
-        imagemBotaoTrocaTema.setAttribute("src", "./assets/projeto-listagem-pokemon/src/imagens/Moon.png");
-    }   
+  const modoEscuroAtivo = body.classList.contains("modo-escuro");
+  body.classList.toggle("modo-escuro");
+  if (modoEscuroAtivo) {
+    imagemBotaoTrocaTema.setAttribute(
+      "src",
+      "./assets/projeto-listagem-pokemon/src/imagens/sun.png"
+    );
+  } else {
+    imagemBotaoTrocaTema.setAttribute(
+      "src",
+      "./assets/projeto-listagem-pokemon/src/imagens/moon.png"
+    );
+  }
 });
